b63013b87d7e0f7fd6f6cc4cec47fdb715a62433
gelald <yb.ng@foxmail.com>
2023-07-05 22:45:22 +0800

    补充注释

--- alternate-print/src/main/java/com/github/gelald/print/PrintTest1.java
+++ alternate-print/src/main/java/com/github/gelald/print/PrintTest1.java
@@ -6,6 +6,13 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * 使用两个线程，交替地打印A、B字符各50次
+ * 难点1:如何确保第一个打印的字符是A
+ * 难点2:如何控制交替打印
+ * 难点3:如何控制打印100个字符
+ * <p>
+ * 扩展点:控制两个服务来交替打印A、B字符(分布式锁)
+ *
  * @author WuYingBin
  * date: 2023/7/4
  */
